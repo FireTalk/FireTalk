26a577582a12a0d1059b821c4b56457446d6a9e0
KwonGiho <shng10503@naver.com>
2016-09-01 00:21:53 +0900

    android studio version update

--- app/src/main/java/com/example/kwongyo/firetalk/activitySupport/ChattingAdapter.java
+++ app/src/main/java/com/example/kwongyo/firetalk/activitySupport/ChattingAdapter.java
@@ -2,6 +2,7 @@ package com.example.kwongyo.firetalk.activitySupport;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,8 @@ import butterknife.ButterKnife;
 /**
  * Created by kwongyo on 2016-08-29.
  */
-public class ChattingAdapter extends CustomAdapter <ChattingData , ChattingAdapter.ViewHolder> {
+public class ChattingAdapter extends CustomAdapter <ChattingData , ChattingAdapter.ChattingViewHolder> {
+    public static String finalSpeakingUser = "";
 
     //provide a suitable cons ( depends on the kind of dataset)
     public ChattingAdapter(Context context, List<ChattingData> lists) {
@@ -30,9 +32,9 @@ public class ChattingAdapter extends CustomAdapter <ChattingData , ChattingAdapt
     * 여기를 잘 꾸며야지 내가 원하는 결과물이 나온다.
     * */
     @Override
-    public ChattingAdapter.ViewHolder onCreateViewHolder(ViewGroup parent , int viewType) {
+    public ChattingAdapter.ChattingViewHolder onCreateViewHolder(ViewGroup parent , int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.catting_another,parent,false);
-        ViewHolder viewHolder = new ViewHolder(v);
+        ChattingViewHolder viewHolder = new ChattingViewHolder(v);
         return viewHolder;
     }
 
@@ -40,33 +42,46 @@ public class ChattingAdapter extends CustomAdapter <ChattingData , ChattingAdapt
     사용자가 스크롤할 때 호출되는 메소드..?
      */
     @Override
-    public void onBindViewHolder(ViewHolder holder,int position) {
-        holder.anotherTextMessage.setText(list.get(position).anotherTextMessage);
-        holder.anotherName.setText(list.get(position).anotherName);
-        // profile image add activity empty
-        Glide.with(context).load(list.get(position).anotherProfileImage).into(holder.anotherProfileImage);
+    public void onBindViewHolder(ChattingViewHolder holder,int position) {
+        String speakingUser = list.get(position).anotherName;
+        if(speakingUser.equals(finalSpeakingUser)) { // 같은 사람이 말 할때 카톡처럼 나오게 하려고..
+            holder.anotherName.setVisibility(View.GONE);
+            holder.anotherTextMessage.setVisibility(View.GONE);
+            holder.anotherName.setVisibility(View.GONE);
+            holder.anotherTextMessageContinue.setVisibility(View.VISIBLE);
 
+            holder.anotherTextMessageContinue.setText(list.get(position).anotherTextMessage);
+        }
+        else { // 같은 사람이 말 하는 경우가 아닌경우임.
+            holder.anotherName.setVisibility(View.VISIBLE);
+            holder.anotherTextMessage.setVisibility(View.VISIBLE);
+            holder.anotherName.setVisibility(View.VISIBLE);
+            holder.anotherTextMessageContinue.setVisibility(View.GONE);
 
+            holder.anotherName.setText(list.get(position).anotherName);
+            // profile image add activity empty
+            Glide.with(context).load(list.get(position).anotherProfileImage).into(holder.anotherProfileImage);
+            holder.anotherTextMessage.setText(list.get(position).anotherTextMessage);
+        }
+        Log.e("ChattingAdapter", "here");
     }
     @Override
     public int getItemCount(){
         return list.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ChattingViewHolder extends RecyclerView.ViewHolder {
         public View view ;
-        @Bind(R.id.another_profile_image)
         public ImageView anotherProfileImage;
-        @Bind(R.id.another_name)
         public TextView anotherName;
-        @Bind(R.id.another_text_message)
         public TextView anotherTextMessage;
-        public ViewHolder(View v) {
+        public TextView anotherTextMessageContinue;
+        public ChattingViewHolder(View v) {
             super(v);
-            view = itemView;
             anotherProfileImage = (ImageView) v.findViewById(R.id.another_profile_image);
             anotherName = (TextView) v.findViewById(R.id.another_name);
             anotherTextMessage = (TextView) v.findViewById(R.id.another_text_message);
+            anotherTextMessageContinue = (TextView) v.findViewById(R.id.another_text_message_continue);
         }
     }
 }
